821441b025e4d48e64d032e24c2d4d667f85ffa2
Matt Seemon <contact@mattseemon.in>
2019-07-07 08:54:32 +0530

    Updated Contact API [SendGrid to Mailgun]
    1. Update Contact API to use mailgun instead of SendGrid
    2. Contact API now sends two emails.  Frist the actual request and second acknowledgement to requestor.
    3. Email template used for Contact Notification & Acknowledgement

--- api/src/contact.js
+++ api/src/contact.js
@@ -1,5 +1,5 @@
 const { json } = require('micro');
-const emailService = require('@sendgrid/mail');
+const mailgun = require('mailgun-js');
 
 module.exports = async (req, res) => {
     const error = (status, err) => {
@@ -12,24 +12,48 @@ module.exports = async (req, res) => {
         error(405, 'Method not allowed');
         return;
     }
+
     const body = await json(req);
-    emailService.setApiKey(process.env.SEND_GRID_API_KEY);
+    const emailService = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
     
-    const emailOptions = {
+    const mailNotification = {
+        from: 'Post Master <postmaster@mattseemon.in>',
+        to: 'contact@mattseemon.in',
+        subject: 'New contact request',
+        template: 'contact-notification',
+        'v:name': body.contact.Name,
+        'v:email': body.contact.Email,
+        'v:message': body.contact.Message,
+        'v:source': body.contact.Source,
+        'v:time': new Date(body.contact.Received).toString()
+    };
+    
+    const mailAcknowledgement = {
+        from: 'Matt Seemon <contact@mattseemon.in>',
         to: body.contact.Email,
-        from: 'contact@mattseemon.in',
-        templateId: 'd-106220c8fe4c42469c64da28ccd80417',
-        dynamic_template_data: {
-            name: body.contact.Name
-        },
+        subject: 'Thank you for contacting me',
+        template: 'contact-acknowledgement',
+        'v:name': body.contact.Name
     };
 
-    emailService.send(emailOptions, (error, result) =>{
+    emailService.messages().send(mailNotification)
+        .then(function(result) {
+            return emailService.messages().send(mailAcknowledgement);
+        })
+        .then(function(result) {
+            res.status = 200;
+            res.end();
+        })
+        .catch((err) => {
+            error(500, err.toString());
+        });
+    /*
+    emailService.messages().send(emailOptions, (error, result) =>{
         if(error) {
             error(500, error.toString());
         } else {
             res.status = 200;
             res.end();
         }
-    });
+    });*/
 };
